a7dd2643b0af757bc43f9e8d6bfaefb00ba9ae88
zhbhun <zhbhun@gmail.com>
2023-09-17 16:18:31 +0800

    fix: integrate eruda for mobile debug

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,3 +1,8 @@
 if (import.meta.env.DEV) {
   window.gtag = () => {}
+  if (typeof new URLSearchParams(location.search).get('debug') === 'string') {
+    import('eruda').then(({ default: eruda }) => {
+      eruda.init()
+    })
+  }
 }
