c901e6d0f8d250df8a16b7c514622cf19a35a954
zhbhun <zhbhun@gmail.com>
2023-11-12 12:19:43 +0800

    fix: cache region

--- packages/common/src/uitls/intl.ts
+++ packages/common/src/uitls/intl.ts
@@ -4,8 +4,18 @@ export function getRegion(): Promise<string> {
   if (pendingRegionRequest) {
     return pendingRegionRequest
   }
+  const region = localStorage.getItem('region')
+  if (region) {
+    return Promise.resolve(region)
+  }
   pendingRegionRequest = fetch('https://api.country.is')
     .then((res) => res.json())
-    .then((data) => data.country)
+    .then((data) => {
+      const region = data.country
+      if (region) {
+        localStorage.setItem('region', region)
+      }
+      return region
+    })
   return pendingRegionRequest
 }
