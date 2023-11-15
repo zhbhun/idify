24fecae3d234dbcf17e1f7edd5ed8938ea96f852
zhbhun <zhbhun@gmail.com>
2023-11-15 08:11:42 +0800

    fix: diable service worker

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['/favicon.ico'],
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['/favicon.ico'],
+    // }),
   ],
   resolve: {
     alias: [
